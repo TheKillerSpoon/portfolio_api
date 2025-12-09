58800d971a268f55667352009a4c3c4d05e7622a
TheKillerSpoon <75366115+TheKillerSpoon@users.noreply.github.com>
2025-12-09 16:35:48 +0100

    0.1.2

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ import express from "express";
 import cors from "cors";
 
 import dbConnectionStatus from "./utils/dbStatus.js";
-
 await dbConnectionStatus();
+
+import languageRoute from "./routes/language.route.js";
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json()); // for parsing application/json
+
+app.use(languageRoute);
 
 // Home route - HTML
 app.get("/", (req, res) => {
@@ -18,7 +21,7 @@ app.get("/", (req, res) => {
     <html>
       <head>
         <meta charset="utf-8"/>
-        <title>Test API</title>
+        <title>portfolio API</title>
       </head>
       <body>
         <h1>API i up and running</h1>
