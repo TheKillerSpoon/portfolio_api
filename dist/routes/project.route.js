b6515ef1b04d1de6ff82e2c09806fffe93018736
TheKillerSpoon <75366115+TheKillerSpoon@users.noreply.github.com>
2025-12-19 15:30:45 +0100

    0.4.3

--- dist/routes/project.route.js
+++ dist/routes/project.route.js
@@ -1,7 +1,26 @@
 import express from "express";
+import { generalMethods } from "./General_routes.js";
+import { getDatabase, collectionExists } from "../utils/dbConnect.js";
+import { z } from "zod";
 import multer from "multer";
-import Project from "../models/project.model.js";
-const projectRoute = express.Router();
+import { ObjectId } from "mongodb";
+// Define schema for test validation
+const schema = z.object({
+    image: z.string(),
+    title: z.string(),
+    description: z.string(),
+    live: z.string().optional(),
+    git: z.string(),
+    languages: z.array(z.instanceof(ObjectId)),
+    showcase: z.boolean().optional().default(false),
+    timeframe: z.string().optional(),
+});
+// Initialize database and collection
+const database = getDatabase();
+collectionExists("project");
+const Collection = database.collection("project");
+// Destructure general methods for the test collection
+const { getAll, getById, create, updateById, deleteById } = generalMethods(Collection, schema);
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "uploads/projects");
@@ -11,117 +30,25 @@ const storage = multer.diskStorage({
     },
 });
 const upload = multer({ storage });
-// get all projects
+const projectRoute = express.Router();
+// get all tests
 projectRoute.get("/projects", async (req, res) => {
-    try {
-        const result = await Project.find({});
-        console.log("Projects found:", result.map((x) => x.title));
-        return res.status(200).send({
-            status: "ok",
-            message: "Projects found!",
-            data: result,
-        });
-    }
-    catch (error) {
-        console.error("Server error", error);
-        return res.status(500).send({
-            status: "error",
-            message: "Server error",
-            error: error.message,
-        });
-    }
+    await getAll(req, res);
 });
-// create a new project
+// get test by id
+projectRoute.get("/project/:id", async (req, res) => {
+    await getById(req, res);
+});
+// create new test
 projectRoute.post("/project", upload.single("image"), async (req, res) => {
-    try {
-        let body = req.body;
-        if (!body.image || !body.title || !body.description) {
-            return res.status(400).send({
-                status: "error",
-                message: `Name and description is required`,
-            });
-        }
-        const newProject = await Project.create(body);
-        return res.status(201).send({
-            status: "ok",
-            message: "Project created successfully!",
-            data: newProject,
-        });
-    }
-    catch (error) {
-        console.error("Something went wrong:", error);
-        return res.status(500).send({
-            status: "error",
-            message: "Something went wrong",
-            error: error.message,
-        });
-    }
+    await create(req, res);
 });
-// update a project by id
-projectRoute.put("/project/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const body = req.body;
-        if (!id) {
-            return res.status(400).send({
-                status: "error",
-                message: "ID is required",
-            });
-        }
-        const updatedProject = await Project.findByIdAndUpdate(id, body, {
-            new: true,
-            runValidators: true,
-        });
-        if (!updatedProject) {
-            return res.status(404).send({
-                status: "error",
-                message: "Project not found",
-            });
-        }
-        return res.status(200).send({
-            status: "ok",
-            message: "Project updated successfully!",
-            data: updatedProject,
-        });
-    }
-    catch (error) {
-        console.error("Server error", error);
-        return res.status(500).send({
-            status: "error",
-            message: "Server error",
-            error: error.message,
-        });
-    }
+// update test by id
+projectRoute.patch("/project/:id", upload.single("image"), async (req, res) => {
+    await updateById(req, res);
 });
-// delete a project by id
-projectRoute.delete("/project/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        if (!id) {
-            return res.status(400).send({
-                status: "error",
-                message: "ID is required",
-            });
-        }
-        const deletedProject = await Project.findByIdAndDelete(id);
-        if (!deletedProject) {
-            return res.status(404).send({
-                status: "error",
-                message: "Project not found",
-            });
-        }
-        return res.status(200).send({
-            status: "ok",
-            message: "Project deleted successfully!",
-        });
-    }
-    catch (error) {
-        console.error("Server error", error);
-        return res.status(500).send({
-            status: "error",
-            message: "Server error",
-            error: error.message,
-        });
-    }
+// delete one or many test by id
+projectRoute.delete("/project", async (req, res) => {
+    await deleteById(req, res);
 });
 export default projectRoute;
